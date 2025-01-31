7240f72fd5cd06f7dc1e5313990a6e447f3cff29
fariafarzana11 <fariamukta00@gmail.com>
2025-01-31 23:07:19 +0600

    hero part updated

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -10,8 +10,7 @@ import './Hero.css';
 const Hero = () => {
 
     return (
-       
-         <div className="max-w-7xl mx-auto">
+        <div className="max-w-7xl mx-auto">
             <Swiper
                 spaceBetween={30}
                 centeredSlides={true}
@@ -29,10 +28,9 @@ const Hero = () => {
                 <SwiperSlide><img src="https://www.buildofy.com/blog/content/images/2022/06/_DSC9610-Edited_-min.jpg" alt="" className='h-[600px] w-[1300px]' /></SwiperSlide>
                 <SwiperSlide><img src="https://www.southernliving.com/thmb/R50HDhYdsNA9mQKzE9zFenYcA4M=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/2270901_cainc0259_1-2000-36b6699219454ee298de1d4565f1ab7d.jpg" alt="" className='h-[600px] w-[1300px]' /></SwiperSlide>
                 <SwiperSlide><img src="https://landmarkhomes.sgp1.digitaloceanspaces.com/images/tongariro/Tongariro-2020.JPG" alt="" className='h-[600px] w-[1300px]' /></SwiperSlide>
-                <SwiperSlide><img src="https://www.nobroker.in/blog/wp-content/uploads/2024/03/best-low-budget-simple-house-design.webp" alt="" className='h-[600px] w-[1300px]'/></SwiperSlide>
+                <SwiperSlide><img src="https://www.nobroker.in/blog/wp-content/uploads/2024/03/best-low-budget-simple-house-design.webp" alt="" className='h-[600px] w-[1300px]' /></SwiperSlide>
             </Swiper>
-        </div> 
-
+        </div>
     )
 }
 export default Hero;
