6f733fcc1d15578b9dc7b1f615c03af1b04378ab
fariafarzana11 <fariamukta00@gmail.com>
2025-02-01 17:20:46 +0600

    updated

--- src/Providers/AuthProviders.jsx
+++ src/Providers/AuthProviders.jsx
@@ -25,7 +25,7 @@ const AuthProvider = ({ children }) => {
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
              setUser(currentUser)
-           console.log(currentUser)
+           //console.log(currentUser)
             if (currentUser) {
                 setUser({
                     uid: currentUser.uid,
